04f0d73edb23cd9a8ec7ca091ba1d6b6869f8232
mimi-star12 <mimivixx12@gmail.com>
2026-01-07 21:44:34 +0300

    lab8 changed

--- lab8/myapp/models/user.py
+++ lab8/myapp/models/user.py
@@ -1,12 +1,18 @@
 class User:
-    def __init__(self, id: int, name: str):
-        self._id = id
+    _next_id = 1  # общий счётчик для всех пользователей
+
+    def __init__(self, name: str):
+        # авто-id
+        self._id = User._next_id
+        User._next_id += 1
+
+        # остальное через setter
         self.name = name
 
     @property
     def id(self) -> int:
-        return self._id
-    
+        return self._id  # read-only
+
 
     @property
     def name(self) -> str:
@@ -14,7 +20,7 @@ class User:
     
     @name.setter
     def name(self, value):
-        if isinstance(value, str) and len(value) > 1:
+        if isinstance(value, str) and value.strip():
             self._name = value
         else:
             raise TypeError('некорректное имя: должно быть строкой и содержать >1 символа')
